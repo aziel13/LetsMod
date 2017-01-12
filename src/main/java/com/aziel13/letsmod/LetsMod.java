5e5d55a89911884d619e27256b53ad827c92882c
aziel13 <avatarofwill13@gmail.com>
2017-01-12 18:25:53 -0500

    First Lets Mod Files commit

---
+++ src/main/java/com/aziel13/letsmod/LetsMod.java
@@ -0,0 +1,34 @@
+package com.aziel13.letsmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Aziel13 on 1/11/2017.
+ */
+
+@Mod(modid="LetsMod", name="LetsMod", version="1.10.2-1.0")
+public class LetsMod {
+
+    @Mod.Instance
+    public static LetsMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent preInit){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent init) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent postInit) {
+
+    }
+
+
+}
